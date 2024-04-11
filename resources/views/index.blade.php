ac6e0293a356c724b50f0ad34c4a3e4a4e27387f
georgiereiss <georgie.reis@outlook.com>
2024-04-11 18:26:30 -0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,6 @@
 @section('content')
 
     <x-page-title title="Ecommerce" pagetitle="Dashboards" />
-
-    <textarea name="tinyeditor" id="tinyeditor" cols="30" rows="10"></textarea>
     
 @endsection
 @push('scripts')
