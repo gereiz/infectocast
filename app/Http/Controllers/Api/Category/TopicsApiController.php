0d01f98c5beed89a9382f5f0434959da550e7929
georgiereiss <georgie.reis@outlook.com>
2024-04-22 08:39:25 -0300

    Ajuste no retorno da endpint de topicos

--- app/Http/Controllers/Api/Category/TopicsApiController.php
+++ app/Http/Controllers/Api/Category/TopicsApiController.php
@@ -10,14 +10,29 @@ class TopicsApiController extends Controller
 {
     // retorna todos os topicos pelo id da subcategoria
     public function getTopics(Request $request) {
-        return response()->json(Topic::where('id_subcategory', $request->id_subcategory)->get(), 200);
+
+        $topics = Topic::where('id_subcategory', $request->id_subcategory)->get();
+
+        $topics_formated = [];
+        foreach ($topics as $topic) {
+            // converte a string em array
+            $topic->plan_id = explode(',', $topic->plan_id);
+            $topics_formated[] = $topic;
+        }
+
+        return response()->json($topics_formated, 200);
     }
 
     // retorna um topico específico pelo request
     public function getTopic(Request $request) {
         $topic = Topic::find($request->id_topic);
         if($topic) {
-            return response()->json($topic, 200);
+            $topic_formated = [];
+            // converte a string em array
+            $topic->plan_id = explode(',', $topic->plan_id);
+            $topic_formated[] = $topic;
+            
+            return response()->json($topic_formated, 200);
         } else {
             return response()->json(['message' => 'Tópico não encontrado'], 404);
         }
