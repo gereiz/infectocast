87c0bdafda250cf15361847ba1d585fba0cc9bdc
georgiereiss <georgie.reis@outlook.com>
2024-04-29 19:10:25 -0300

    Update CalculatorService.php

--- app/Services/Api/CalculatorService.php
+++ app/Services/Api/CalculatorService.php
@@ -8,7 +8,7 @@ namespace App\Services\Api;
 class CalculatorService
 {
     public function clearenceOfCreatine(int $idade, int $peso, int $creatinina, string $sexo) {
-        if($sexo == 'M') {
+        if($sexo == 'Masc.') {
             $clearence = number_format(((140 - $idade) * $peso) / (72 * ($creatinina / 100)), 2);
         } else {
             $clearence = number_format((((140 - $idade) * $peso) / (72 * ($creatinina / 100)) * 0.85), 2);
