94b925c3bceb2774280a5fe6c56df897a9812fcc
georgiereiss <georgie.reis@outlook.com>
2024-05-27 21:47:04 -0300

    Ajustes na configuração de email

--- app/Services/Api/CalculatorService.php
+++ app/Services/Api/CalculatorService.php
@@ -6,7 +6,7 @@ namespace App\Services\Api;
 
 
 class CalculatorService
-{
+{   
     public function clearenceOfCreatine($idade,  $peso, $creatinina, $sexo) {
         if($sexo == 'Masc.') {
             $clearence = number_format(((140 - $idade) * $peso) / (72 * ($creatinina / 100)), 2);
